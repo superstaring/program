0bf3b4339089d5fa0cbe1f75de03538e05eb38cd
1690133293@qq.com <1690133293@qq.com>
2019-07-19 17:31:14 +0800

    first commit

---
+++ pages/article/article.js
@@ -0,0 +1,90 @@
+// pages/article/article.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    lists: [{ id: '0', title: "王先生" }, { id: '1', title: '李先生' }, { id: '1', title: '陈先生' }],
+    todo:[]
+  },
+  back: function () {
+    wx.navigateBack({
+      delta:1
+    })
+  }, 
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that=this;
+    wx.request({
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      method:'get',
+      success:function(data){
+        that.setData({
+          todo:data.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    if (typeof this.getTabBar === 'function' &&
+      this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 1
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+
+})
+
+
---
+++ pages/logs/logs.js
@@ -0,0 +1,25 @@
+//logs.js
+const util = require('../../utils/util.js')
+
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function() {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 3
+      })
+    }
+  }
+})
